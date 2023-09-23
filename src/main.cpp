1d7555bbf22143a4d379d32b54f5782a8785150a
hotgluebanjo <66244111+hotgluebanjo@users.noreply.github.com>
2023-09-23 11:28:05 +0000

    Concat

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+// seyrek, ritka
+#include <cassert>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,29 +10,40 @@
 
 using namespace alglib;
 
+// TODO: Consistency.
 void linspace(real_1d_array *res, double start, double end, size_t steps) {
     double delta = (end - start) / double(steps - 1);
-    double *buf;
-    // Am I supposed to use `new` here?
-    // `buf = new double[steps];`
-    // buf = (double *)calloc(steps, sizeof(*buf));
-    buf = new double[steps];
+    double *buf = new double[steps];
 
     for (int i = 0; i < steps; i += 1) {
         buf[i] = double(i) * delta + start;
     }
 
     res->setcontent(steps, buf);
-    // free(buf);
-    delete buf;
+    delete[] buf;
 }
 
-
-// ALGLIB expects a 2D array in the format:
+// ALGLIB's 3D RBF expects a 2D array in the format:
 // [xyz.x, xyz.y, xyz.z, f(xyz).x, f(xyz).y, f(xyz).z]
 // or
 // [source.x, source.y, source.z, target.x, target.y, target.z]
-void concatenate_xy(real_1d_array x, real_1d_array y) {
+void concat_real_2d_array(real_2d_array x, real_2d_array y, real_2d_array *res) {
+    assert(x.rows() == y.rows());
+    assert(x.cols() == 3);
+    assert(y.cols() == 3);
+
+    ae_int_t n_rows = x.rows();
+    ae_int_t n_cols = x.cols();
+    res->setlength(n_rows, 2 * n_cols);
+
+    for (ae_int_t i = 0; i < n_rows; i += 1) {
+        for (ae_int_t j = 0; j < n_cols; j += 1) {
+            (*res)[i][j] = x[i][j];
+        }
+        for (ae_int_t j = n_cols; j < 2 * n_cols; j += 1) {
+            (*res)[i][j] = y[i][j - n_cols];
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -52,13 +65,32 @@ int main(int argc, char **argv) {
     real_1d_array x = "[+1,+1]";
     real_1d_array res;
     rbfcalc(model, x, res);
-    // rbfgridcalc2v(model,);
+
+    real_1d_array grid;
+    linspace(&grid, 0.0, 1.0, 10);
+    // rbfgridcalc2v(model, );
 
     std::cout << res.tostring(2) << std::endl; // EXPECTED: [0.000,-1.000]
 
     real_1d_array thing;
     linspace(&thing, 0.0, 1.0, 5);
     std::cout << thing.tostring(2) << std::endl;
+
+    real_2d_array bla_x =
+        "[[1, 2, 3],"
+         "[1, 2, 3],"
+         "[1, 2, 3],"
+         "[1, 2, 3]]";
+
+    real_2d_array bla_y =
+        "[[4, 5, 6],"
+         "[4, 5, 6],"
+         "[4, 5, 6],"
+         "[4, 5, 6]]";
+
+    real_2d_array resbla;
+    concat_real_2d_array(bla_x, bla_y, &resbla);
+    std::cout << resbla.tostring(3) << std::endl;
 
     return 0;
 }
