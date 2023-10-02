5c158b95856c8a379cfe2ad035c29a3bb436f1b7
hotgluebanjo <66244111+hotgluebanjo@users.noreply.github.com>
2023-10-02 10:28:54 +0000

    Build note

--- build.ps1
+++ build.ps1
@@ -3,12 +3,12 @@ $build_lib = $false
 $flags = "-O3"
 
 if ($build_lib) {
-    # zig c++ doesn't work with wildcards and with -c doesn't accept multiple inputs.
+    # zig c++ -c doesn't work with multiple inputs.
     foreach ($f in (dir ./alglib/*.cpp | select basename,extension)) {
         zig c++ $flags -c "./alglib/$($f.basename).cpp" -o "./target/alglib/$($f.basename).obj"
     }
 }
 
-# Won't link combined.
+# Won't link concatenated for MinGW with -O3. TODO: concatenate dbg build.
 zig c++ $flags -I . -c ./src/main.cpp -o ./target/main.obj
-zig c++ $flags ./target/main.obj $(dir ./target/alglib/*.obj) -o ./target/sdfit.exe
+zig c++ $flags ./target/main.obj (dir ./target/alglib/*.obj) -o ./target/sdfit.exe
