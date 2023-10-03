27732fe0fc5ce0d0a5d34c1eef0a92946247d16b
hotgluebanjo <66244111+hotgluebanjo@users.noreply.github.com>
2023-10-03 23:50:41 +0000

    Update build

--- build.ps1
+++ build.ps1
@@ -1,14 +1,35 @@
-$build_lib = $false
+# First
+#    build.ps1 lib
+# then
+#    build.ps1 release x.y.z
+# or
+#    build.ps1
+# for debug build.
 
-$flags = "-O3"
-
-if ($build_lib) {
+if ($args[0] -eq "lib") {
     # zig c++ -c doesn't work with multiple inputs.
-    foreach ($f in (dir ./alglib/*.cpp | select basename,extension)) {
-        zig c++ $flags -c "./alglib/$($f.basename).cpp" -o "./target/alglib/$($f.basename).obj"
+    foreach ($f in (dir ./alglib/*.cpp | select name)) {
+        zig c++ -O3 -c "./alglib/$($f.name).cpp" -o "./target/alglib/$($f.name).obj"
     }
-}
+} elseif ($args[0] -eq "release") {
+    if ($args.length -eq 1) {
+        echo "Missing version"
+        exit
+    }
 
-# Won't link concatenated for MinGW with -O3. TODO: concatenate dbg build.
-zig c++ $flags -I . -c ./src/main.cpp -o ./target/main.obj
-zig c++ $flags ./target/main.obj (dir ./target/alglib/*.obj) -o ./target/sdfit.exe
+    $version = $args[1]
+
+    # Won't link concatenated with -O3. MinGW issue.
+    zig c++ -I . -O3 -c ./src/main.cpp -o ./target/main.obj
+    zig c++ -O3 ./target/main.obj (dir ./target/alglib/*.obj) -o ./release/windows/sdfit.exe
+
+    # Wildcards don't work of course.
+    zig c++ -I . -O3 --target=x86_64-macos (dir ./src/*.cpp) (dir ./alglib/*.cpp) -o release/macos/sdfit
+    zig c++ -I . -O3 --target=x86_64-linux (dir ./src/*.cpp) (dir ./alglib/*.cpp) -o release/linux/sdfit
+
+    compress-archive release/windows/sdfit.exe "release/sdfit-$version-windows.zip"
+    tar -czf "./release/sdfit-$version-macos.tar.gz" ./release/macos/sdfit
+    tar -czf "./release/sdfit-$version-linux.tar.gz" ./release/linux/sdfit
+} else {
+    zig c++ -I . ./src/main.cpp (dir ./target/alglib/*.obj) -o ./target/sdfit.exe
+}
--- release.ps1
+++
@@ -1,3 +0,0 @@
-# Wildcards don't work of course.
-zig c++ -I . -O3 --target=x86_64-linux (dir ./src/*.cpp) (dir ./alglib/*.cpp) -o release/linux/sdfit
-zig c++ -I . -O3 --target=x86_64-macos (dir ./src/*.cpp) (dir ./alglib/*.cpp) -o release/macos/sdfit
